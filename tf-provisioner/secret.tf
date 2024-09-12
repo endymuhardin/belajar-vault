536930554515d63a1e56e8f3c1e33c6455bc1185
Endy Muhardin <endy.muhardin@gmail.com>
2024-09-12 14:01:47 +0700

    edit credentials

--- tf-provisioner/secret.tf
+++ tf-provisioner/secret.tf
@@ -2,9 +2,9 @@ resource "vault_generic_secret" "aplikasi-belajar" {
   path = "secret/aplikasi-belajar"
   data_json = <<EOF
     {
-        "spring.datasource.url" : "jdbc:postgresql://localhost/belajar-vault",
-        "spring.datasource.username" : "belajar",
-        "spring.datasource.password" : "belajar-vault-123"
+        "spring.datasource.url" : "jdbc:postgresql://localhost/belajar-vault-k8s",
+        "spring.datasource.username" : "belajar-k8s",
+        "spring.datasource.password" : "belajar-vault-123-k8s"
     }
   EOF
 }
